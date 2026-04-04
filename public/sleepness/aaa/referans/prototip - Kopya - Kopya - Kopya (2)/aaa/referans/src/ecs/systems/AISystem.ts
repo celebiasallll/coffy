20ac66feca916a2a9a2d1ebe54bfabb81f64155c
celebiasallll <nirvanaofis7@gmail.com>
2026-04-04 21:55:18 +0300

    Remove unnecessary reference folder in sleepness

--- public/sleepness/aaa/referans/prototip - Kopya - Kopya - Kopya (2)/aaa/referans/src/ecs/systems/AISystem.ts
+++
@@ -1,4 +0,0 @@
-import { IWorld } from 'bitecs';
-// AI sistemi NPCSystem.ts tarafından yönetiliyor.
-// ECS entegrasyonu sonraki versiyonda yapılacak.
-export const aiSystem = (_world: IWorld): IWorld => _world;
